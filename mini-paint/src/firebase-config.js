22cde8aeb72e207864fcbf3db39be7053cfbce61
Krystsina Kavalevich <krikoax@gmail.com>
2023-01-17 14:06:18 +0100

    feat: add firebase config file

---
+++ mini-paint/src/firebase-config.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCX2xTIjDS4rU4_3xRekai3gOMnjhauEaE",
+  authDomain: "mini-paint-2566a.firebaseapp.com",
+  projectId: "mini-paint-2566a",
+  storageBucket: "mini-paint-2566a.appspot.com",
+  messagingSenderId: "788461609145",
+  appId: "1:788461609145:web:446dc44daf4f37b9053b68",
+};
+
+firebase.initializeApp(firebaseConfig);
